f897bd99eebbad90da3ca9efea3465824ceb9179
t3ja <raviteja.mnnit@gmail.com>
2019-03-13 12:12:32 +0530

    Models

--- analyser/models.py
+++ analyser/models.py
@@ -1,3 +1,39 @@
+from typing import Tuple
+
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+COUNTRIES = (
+        ('uk', 'UK'),
+        ('us', 'USA'),
+        ('in', 'India'),
+    )
+LABELS = (
+    ('0', 'NEGATIVE'),
+    ('1', 'POSITIVE'),
+    ('2', 'NEUTRAL'),
+)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=1000)
+    content = models.CharField(max_length=10000)
+    country = models.CharField(max_length=2, choices=COUNTRIES)
+    pub_date = models.DateTimeField('date published', blank=True)
+    analysed_at = models.DateTimeField('date article created', blank=True)
+
+    label = models.CharField(max_length=1, choices=LABELS)
+
+    def __str__(self):
+        return self.content
+
+
+class Choice(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    chosen_label = models.CharField(max_length=1, choices=LABELS)
+    votes = models.IntegerField(default=0)
+    created = models.DateTimeField('date article created')
+    updated = models.DateTimeField('date article updated')
+
--- sentimentalnews/settings.py
+++ sentimentalnews/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'analyser',
+    'analyser.apps.AnalyserConfig',
     'crawler',
     'reports'
 ]
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
